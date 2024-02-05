b4121d08233e58f5cd1f2a666cade547e3dad668
Diogo Pincetta <diogohe08@gmail.com>
2024-02-05 13:00:23 -0300

    Exercicio 1

---
+++ exercicio1.java
@@ -0,0 +1,26 @@
+package estrutura_condicional;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int x = 5;
+		
+		System.out.println("Bom dia");
+		
+		if(x>0) {
+		   System.out.println("Boa Tarde");
+		}
+		
+		System.out.println("Boa noite");
+		
+		sc.close();
+		
+
+	}
+
+}
